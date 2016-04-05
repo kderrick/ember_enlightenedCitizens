fe325effde5b0325bbc1d14403070970b73b439e
chris berry and Kyle Derrick <kderrick1988@gmail.com>
2016-04-05 10:17:05 -0700

    add youtube and twitter contact for legislator lookup form

--- app/routes/results.js
+++ app/routes/results.js
@@ -5,8 +5,9 @@ export default Ember.Route.extend({
  model: function(params) {
    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=64258e8ddf2240809fd1cd35b593dd11&zip=' + params.zip;
    return Ember.$.getJSON(url).then(function(responseJSON) {
-     console.log(responseJSON.results[0]);
+     console.log(responseJSON.results);
      return responseJSON.results;
    });
   },
 });
+// 'http://congress.api.sunlightfoundation.com/committees/locate?apikey=64258e8ddf2240809fd1cd35b593dd11'

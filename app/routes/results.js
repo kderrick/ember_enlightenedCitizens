1d0b2ca42949236dac8430911577d1b48cc0e9e8
chris berry and Kyle Derrick <kderrick1988@gmail.com>
2016-04-05 14:29:27 -0700

    add .env file to remove hardcoded api

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,9 +1,11 @@
+import config from '../config/environment';
 import Ember from 'ember';
+var key = config.myApiKey;
 
 
 export default Ember.Route.extend({
  model: function(params) {
-   var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=64258e8ddf2240809fd1cd35b593dd11&zip=' + params.zip;
+   var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey='+ key +'&zip=' + params.zip;
    return Ember.$.getJSON(url).then(function(responseJSON) {
      console.log(responseJSON.results);
      return responseJSON.results;
